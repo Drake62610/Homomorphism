c83d616e13e82ecd33f9b7eb90909984538d7702
VictorienEstNull <32387547+VictorienEstNull@users.noreply.github.com>
2018-04-03 22:17:45 +0200

    Add files via upload

---
+++ CipherBit.cpp
@@ -0,0 +1,71 @@
+#include "CipherBit.h"
+#include <seal/seal.h>
+
+using namespace std;
+using namespace seal;
+
+//Constructor
+CipherBit::CipherBit(seal::Evaluator &eva, seal::Ciphertext a){
+  evaluator = &eva;
+  cipherBit = a;
+}
+
+//Getter
+Ciphertext CipherBit::getcipherBit(){
+  return this->cipherBit;
+}
+Evaluator* CipherBit::getEvaluator(){
+  return this->evaluator;
+}
+//Setter
+void CipherBit::setcipherBit(Ciphertext a){
+  this -> cipherBit = a;
+}
+
+//Methods
+/*
+  Input : b is the Cipherbit to XOR to this->cipherBit
+  this->cipherBit will be override by the result
+  XOR will be comuted by (a-b)^2
+*/
+void CipherBit::XOR(CipherBit b){
+  Ciphertext xored = b.getcipherBit(); //We get the ciphertext of the CipherBit
+  (this -> evaluator) -> negate(xored);
+  (this -> evaluator) -> add(this -> cipherBit,xored);
+  (this -> evaluator) -> square(this -> cipherBit); //Now this->cipherBit has the XORed result
+}
+
+/*
+  Input : b is the Cipherbit to add to this->cipherBit
+  Output : The carry as a Cipherbit
+*/
+CipherBit CipherBit::add(CipherBit b){
+  Ciphertext adding = b.getcipherBit();
+  Ciphertext copy = this -> cipherBit;
+  this -> XOR(b);
+  (this -> evaluator) -> multiply(copy,adding);
+  CipherBit result(*evaluator,(Ciphertext)copy);
+  return result;
+}
+
+void CipherBit::notLogic(){
+  Plaintext myPlaintext("1");
+  Ciphertext myCipher; 
+  (this -> encryptor).encrypt(myPlaintext,myCipher);
+  (this -> evaluator) -> sub(myCipher,this->cipherBit);
+  (this->cipherBit)=myCipher;
+}
+
+CipherBit strictPlusPetit(CipherBit a,CipherBit b){
+	CipherBit tmp=a;
+	tmp.notLogic();
+	(this -> evaluator) -> multiply(b.getcipherBit);
+	return tmp;
+}
+
+void CipherBit::negate(){
+
+}
+CipherBit multiply(CipherBit b){
+  return b;
+}
---
+++ main.cpp
@@ -0,0 +1,116 @@
+#include <vector>
+#include <seal/seal.h>
+#include <../Structures/CipherBit.h>
+
+using namespace std;
+using namespace seal;
+void test_xor1bit(Evaluator evaluator, Decryptor decryptor,Encryptor encryptor){
+  cout << "Table de vérité du XOR sur 1 bit" << endl;
+  Plaintext myPlaintext1("1");Plaintext myPlaintext0("0");
+  Plaintext plainResult("0");
+  Ciphertext myCipher1; encryptor.encrypt(myPlaintext1,myCipher1);
+  Ciphertext myCipher0; encryptor.encrypt(myPlaintext0,myCipher0);
+  CipherBit myCipherBit1(evaluator, myCipher1);
+  CipherBit myCipherBit0(evaluator, myCipher0);
+
+  //Tests
+  CipherBit result = myCipherBit0;
+  result.XOR(myCipherBit0);
+  decryptor.decrypt(result.getcipherBit(),plainResult);
+  cout << "0 XOR 0 = " << plainResult.to_string() << endl;
+
+  result.XOR(myCipherBit1);
+  decryptor.decrypt(result.getcipherBit(),plainResult);
+  cout << "0 XOR 1 = " << plainResult.to_string() << endl;
+
+  result = myCipherBit1;
+  result.XOR(myCipherBit0);
+  decryptor.decrypt(result.getcipherBit(),plainResult);
+  cout << "1 XOR 0 = " << plainResult.to_string() << endl;
+
+  result.XOR(myCipherBit1);
+  decryptor.decrypt(result.getcipherBit(),plainResult);
+  cout << "1 XOR 1 = " << plainResult.to_string() << endl;
+}
+void test_add1bit(Evaluator evaluator, Decryptor decryptor,Encryptor encryptor){
+  cout << "Table de vérité du + sur 1 bit" << endl;
+  Plaintext myPlaintext1("1");Plaintext myPlaintext0("0");
+  Plaintext plainResult("0"); Plaintext plainCarry("0");
+  Ciphertext myCipher1; encryptor.encrypt(myPlaintext1,myCipher1);
+  Ciphertext myCipher0; encryptor.encrypt(myPlaintext0,myCipher0);
+  CipherBit myCipherBit1(evaluator, myCipher1);
+  CipherBit myCipherBit0(evaluator, myCipher0);
+  CipherBit cipherCarry(evaluator, myCipher0);
+
+  //Tests
+  CipherBit result = myCipherBit0;
+  cipherCarry = result.add(myCipherBit0);
+  decryptor.decrypt(result.getcipherBit(),plainResult);
+  decryptor.decrypt(cipherCarry.getcipherBit(),plainCarry);
+  cout << "0 + 0 = " << plainResult.to_string() << "  carry = " << plainCarry.to_string() << endl;
+
+  cipherCarry = result.add(myCipherBit1);
+  decryptor.decrypt(result.getcipherBit(),plainResult);
+  decryptor.decrypt(cipherCarry.getcipherBit(),plainCarry);
+  cout << "0 + 1 = " << plainResult.to_string() << "  carry = " << plainCarry.to_string() << endl;
+
+  result = myCipherBit1;
+  cipherCarry = result.add(myCipherBit0);
+  decryptor.decrypt(result.getcipherBit(),plainResult);
+  decryptor.decrypt(cipherCarry.getcipherBit(),plainCarry);
+  cout << "1 + 0 = " << plainResult.to_string() << "  carry = " << plainCarry.to_string() << endl;
+
+  cipherCarry = result.add(myCipherBit1);
+  decryptor.decrypt(result.getcipherBit(),plainResult);
+  decryptor.decrypt(cipherCarry.getcipherBit(),plainCarry);
+  cout << "1 + 1 = " << plainResult.to_string() << "  carry = " << plainCarry.to_string() << endl;
+}
+
+void test_plusPetit(Evaluator evaluator, Decryptor decryptor,Encryptor encryptor){
+  cout << "test plus petit 1 bit" << endl;
+  Plaintext myPlaintext1("1");Plaintext myPlaintext0("0");
+  Plaintext plainResult("0");
+  Ciphertext myCipher1; encryptor.encrypt(myPlaintext1,myCipher1);
+  Ciphertext myCipher0; encryptor.encrypt(myPlaintext0,myCipher0);
+  CipherBit myCipherBit1(evaluator, myCipher1);
+  CipherBit myCipherBit0(evaluator, myCipher0);
+
+  //TESTS
+  CipherBit result = strictPlusPetit(myCipherBit0,myCipherBit1);
+  cout << "0 < 1 = " << plainResult.to_string() << endl;
+}
+
+int main(){
+  //Configuration des paramètres homomorphiques
+    EncryptionParameters parms;
+    parms.set_poly_modulus("1x^4096 + 1");
+    parms.set_coeff_modulus(coeff_modulus_128(4096));
+    parms.set_plain_modulus(40961);
+  //Validation des paramètres et création du contexte
+    SEALContext context(parms);
+  //Generation des clées publiques et privées
+    KeyGenerator keygen(context);
+    PublicKey public_key = keygen.public_key();
+    SecretKey secret_key = keygen.secret_key();
+    GaloisKeys gal_keys;
+    keygen.generate_galois_keys(30, gal_keys);
+    EvaluationKeys ev_keys;
+    keygen.generate_evaluation_keys(30, ev_keys);
+  //Homomorphic Objects
+    Encryptor encryptor(context, public_key);
+    Evaluator evaluator(context);
+    Decryptor decryptor(context, secret_key);
+
+  //Test
+  Plaintext myPlaintext1("1");
+  Plaintext myPlaintext0("0");
+  //Ciphertext myCipher; encryptor.encrypt(myPlaintext1,myCipher);
+  //CipherBit myFirstBit(evaluator, myCipher);
+  //decryptor.decrypt(myFirstBit.getcipherBit(),myPlaintext1);
+  //cout << myPlaintext1.to_string() <<endl;
+  //test_xor1bit(evaluator,decryptor,encryptor);
+  //test_add1bit(evaluator,decryptor,encryptor);
+  test_plusPetit(evaluator,decryptor,encryptor);
+
+  return 0;
+}
