2f71bcfba4c6913235203681443f1f641ad7f14d
MatteFlorian <matteflorian@live.fr>
2018-04-05 19:13:59 +0200

    Noise work

--- Structures/CipherBit.h
+++ Structures/CipherBit.h
@@ -8,13 +8,20 @@
 class CipherBit{
 public:
   //Constructor
+  /*
+    Input : evaluator and encryptor from the seal context, needed to use public key and do seal Operations
+            Ciphertext to stock in this CipherBit
+    Noise : no noise are consumed in this constructor
+  */
   CipherBit(seal::Evaluator &evaluator,seal::Encryptor &encryptor, seal::Ciphertext);
   //Getter
   seal::Ciphertext getcipherBit();
   seal::Evaluator* getEvaluator();
   //Setter
   void setcipherBit(seal::Ciphertext a);
+
   //Methods
+  void XORdebug(CipherBit b, seal::Decryptor decryptor, seal::EvaluationKeys ev_keys);
   /*
     Input : b is the Cipherbit to XOR to this->cipherBit
     this->cipherBit will be override by the result
